834647fa75c530a759ed16f5a10597a70dea54e4
Sameer-EI <sameerul@gmail.com>
2026-02-05 14:38:27 +0530

    Update constants.jsx

--- src/global/constants.jsx
+++ src/global/constants.jsx
@@ -1,7 +1,7 @@
 export const constants = {
   isOnline: true,
   isOffline: true,
-  baseUrl: "https://api.newprogressive.in", //deployed for school newprogressive PROD 
+  baseUrl: "https://api.newprogressive.in", //deployed for school newprogressive PRODUCTION
   // baseUrl: "https://smsproject1.pythonanywhere.com/", //deployed for testing
   // baseUrl: "https://smsproject.pythonanywhere.com/", //deployed for school NPEPS deployed
   //baseUrl: "https://newprogressive.pythonanywhere.com/", //deployed for school newprogressive testing
@@ -9,12 +9,9 @@ export const constants = {
   // baseUrl: "https://superkidsschool.pythonanywhere.com/", //deployed for school SUPER KIDS
   // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms/", //farha
   //  baseUrl: "https://9gqxjbjg-8000.inc1.devtunnels.ms/", //tahur
-  // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen
-  // baseUrl: "https://2gqlk571-8000.inc1.devtunnels.ms", //saqib
   // baseUrl:"https://958cp4w5-8000.inc1.devtunnels.ms/", //saba
   // baseUrl: "https://94f38xkg-8000.inc1.devtunnels.ms", //naaz
   // baseUrl: "https://94f38xkg-7000.inc1.devtunnels.ms/", //naaz
-  //  baseUrl:"https://2lw4clk2-8000.inc1.devtunnels.ms", //shahbaz
   //  baseUrl:"https://187gwsw1-8000.inc1.devtunnels.ms/", //farha
 
   hideEdgeRevealStyle: `
